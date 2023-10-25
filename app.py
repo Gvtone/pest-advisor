761aa4bb924be3966f50d580cddd100a586ab5ee
Gerald Tolentino <geraldtolentino82@yahoo.com.ph>
2023-10-26 02:54:32 +0800

    Added new routes and implemented image upload

--- app.py
+++ app.py
@@ -1,17 +1,19 @@
 import os
-from flask import Flask, flash, redirect, render_template, request, session
+from flask import Flask, flash, redirect, render_template, request, session, jsonify, url_for
 from flask_session import Session
 from tempfile import mkdtemp
 from werkzeug.security import check_password_hash, generate_password_hash
+from werkzeug.utils import secure_filename
 
+# Global Variables
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 
 # Configure application
 app = Flask(__name__)
 
 # Ensure templates are auto-reloaded
 app.config["TEMPLATES_AUTO_RELOAD"] = True
-
-
+app.config['UPLOAD_FOLDER'] = "./captured"
 
 # Configure session to use filesystem (instead of signed cookies)
 app.config["SESSION_PERMANENT"] = False
@@ -53,3 +55,33 @@ def monitor():
 def farm():
     # Returning user's portfolio
     return render_template("farm.html")
+
+@app.route("/organism")
+def organism():
+    # Returning user's portfolio
+    return render_template("organism.html")
+
+
+# For receiving images from the Orange Pi PC
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/upload_image', methods=['GET', 'POST'])
+def upload_image():
+    # Receive the image data
+    if 'file' not in request.files:
+        flash('Invalid file')
+        return redirect(request.url)
+    file = request.files['file']
+
+    if file.filename == '':
+        flash('No selected file')
+        return redirect(request.url)
+    
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+    # Respond with a success message
+    return jsonify({"message": "Image received and saved successfully"})
